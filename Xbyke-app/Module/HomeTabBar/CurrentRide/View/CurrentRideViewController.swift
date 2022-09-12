2094e632b02c3d77b5faee079e6ddb771f373691
Christian <yoh.chrisitan@gmail.com>
2022-09-12 09:37:45 -0400

    feature/hometabbar/items-ui-details

---
+++ Xbyke-app/Module/HomeTabBar/CurrentRide/View/CurrentRideViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentRideViewController.swift
+//  Xbyke-app
+//
+//  Created by Christian Calixto on 12/9/22.
+//
+
+import UIKit
+
+class CurrentRideViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+
+    }
+    
+}
